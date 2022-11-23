ff4893462d53576200cd55dcdc885eab9cb0e681
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-11-23 03:37:08 +0200

    Format

--- src/fastq_streams.rs
+++ src/fastq_streams.rs
@@ -1,46 +1,44 @@
-use std::io;
 use flate2::read::GzDecoder;
-use seq_io::fastq::Reader as seqio_fastq_reader;
-use seq_io::fastq::Record as seqio_fastq_record;
 use seq_io::fasta::Reader as seqio_fasta_reader;
 use seq_io::fasta::Record as seqio_fasta_record;
+use seq_io::fastq::Reader as seqio_fastq_reader;
+use seq_io::fastq::Record as seqio_fastq_record;
 use std::fs::File;
+use std::io;
 
 // Fasta or fastq stream
-pub trait SeqStream{
+pub trait SeqStream {
     fn read_all(&mut self);
 }
 
 // Template class for a fastq stream that takes the raw data stream as a template parameter
-pub struct FastqStream<T: std::io::Read>{
-    reader: seqio_fastq_reader<T>
+pub struct FastqStream<T: std::io::Read> {
+    reader: seqio_fastq_reader<T>,
 }
 
 // Template class for a fasta stream that takes the raw data stream as a template parameter
-pub struct FastaStream<T: std::io::Read>{
-    reader: seqio_fasta_reader<T>
+pub struct FastaStream<T: std::io::Read> {
+    reader: seqio_fasta_reader<T>,
 }
 
-impl<T: std::io::Read> FastqStream<T>{
-    pub fn new(stream: T) -> FastqStream<T>{
-        return FastqStream::<T>{
-            reader: seqio_fastq_reader::<T>::new(stream)
+impl<T: std::io::Read> FastqStream<T> {
+    pub fn new(stream: T) -> FastqStream<T> {
+        return FastqStream::<T> {
+            reader: seqio_fastq_reader::<T>::new(stream),
         };
     }
 }
 
-impl<T: std::io::Read> FastaStream<T>{
-    pub fn new(stream: T) -> FastaStream<T>{
-        return FastaStream::<T>{
-            reader: seqio_fasta_reader::<T>::new(stream)
+impl<T: std::io::Read> FastaStream<T> {
+    pub fn new(stream: T) -> FastaStream<T> {
+        return FastaStream::<T> {
+            reader: seqio_fasta_reader::<T>::new(stream),
         };
     }
 }
 
-
-impl<T: std::io::Read> SeqStream for FastqStream<T>{
-
-    fn read_all(&mut self){
+impl<T: std::io::Read> SeqStream for FastqStream<T> {
+    fn read_all(&mut self) {
         let mut sum: i64 = 0;
         while let Some(record) = self.reader.next() {
             let record = record.expect("Error reading record");
@@ -50,27 +48,24 @@ impl<T: std::io::Read> SeqStream for FastqStream<T>{
     }
 }
 
-
-impl FastqStream<File>{
-    pub fn new_from_file(filename: &String) -> FastqStream<File>{
-        return FastqStream::<File>{
-            reader: seqio_fastq_reader::new(File::open(&filename).unwrap())
-        };
-    }
-
-}
-
-impl FastqStream<GzDecoder<File>>{
-    pub fn new_from_file(filename: &String) -> FastqStream<GzDecoder<File>>{
-        return FastqStream::<GzDecoder<File>>{
-            reader: seqio_fastq_reader::new(GzDecoder::new(File::open(&filename).unwrap()))
+impl FastqStream<File> {
+    pub fn new_from_file(filename: &String) -> FastqStream<File> {
+        return FastqStream::<File> {
+            reader: seqio_fastq_reader::new(File::open(&filename).unwrap()),
         };
     }
 }
 
-impl<T: std::io::Read> SeqStream for FastaStream<T>{
+impl FastqStream<GzDecoder<File>> {
+    pub fn new_from_file(filename: &String) -> FastqStream<GzDecoder<File>> {
+        return FastqStream::<GzDecoder<File>> {
+            reader: seqio_fastq_reader::new(GzDecoder::new(File::open(&filename).unwrap())),
+        };
+    }
+}
 
-    fn read_all(&mut self){
+impl<T: std::io::Read> SeqStream for FastaStream<T> {
+    fn read_all(&mut self) {
         let mut sum: i64 = 0;
         while let Some(record) = self.reader.next() {
             let record = record.expect("Error reading record");
@@ -80,21 +75,18 @@ impl<T: std::io::Read> SeqStream for FastaStream<T>{
     }
 }
 
-
-impl FastaStream<File>{
-    pub fn new_from_file(filename: &String) -> FastaStream<File>{
-        return FastaStream::<File>{
-            reader: seqio_fasta_reader::new(File::open(&filename).unwrap())
+impl FastaStream<File> {
+    pub fn new_from_file(filename: &String) -> FastaStream<File> {
+        return FastaStream::<File> {
+            reader: seqio_fasta_reader::new(File::open(&filename).unwrap()),
         };
     }
 }
 
-impl FastaStream<GzDecoder<File>>{
-    pub fn new_from_file(filename: &String) -> FastaStream<GzDecoder<File>>{
-        return FastaStream::<GzDecoder<File>>{
-            reader: seqio_fasta_reader::new(GzDecoder::new(File::open(&filename).unwrap()))
+impl FastaStream<GzDecoder<File>> {
+    pub fn new_from_file(filename: &String) -> FastaStream<GzDecoder<File>> {
+        return FastaStream::<GzDecoder<File>> {
+            reader: seqio_fasta_reader::new(GzDecoder::new(File::open(&filename).unwrap())),
         };
     }
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,115 @@
 extern crate flate2;
 
-use crate::fastq_streams::{SeqStream,FastqStream,FastaStream};
+use crate::fastq_streams::{FastaStream, FastqStream, SeqStream};
 
 mod fastq_streams;
 
-use std::io;
+use clap::{Arg, ArgAction, Command};
 use flate2::read::GzDecoder;
-use std::fs::File;
 use std::env;
-use std::path::Path;
 use std::ffi::OsStr;
-use clap::{Arg,Command,ArgAction};
+use std::fs::File;
+use std::io;
+use std::path::Path;
 
 struct SeqReader {
-    stream: Box<dyn SeqStream>
+    stream: Box<dyn SeqStream>,
 }
 
-impl SeqReader{
-
+impl SeqReader {
     // New from file
-    pub fn new(filename: &String) -> SeqReader{
-        if filename.ends_with("fastq.gz"){
-            return SeqReader {stream: Box::new(FastqStream::<GzDecoder<File>>::new_from_file(filename))};
-        } else if filename.ends_with("fastq"){
-            return SeqReader {stream: Box::new(FastqStream::<File>::new_from_file(filename))};
-        } else if filename.ends_with("fna.gz"){
-            return SeqReader {stream: Box::new(FastaStream::<GzDecoder<File>>::new_from_file(filename))};
-        } else if filename.ends_with("fna"){
-            return SeqReader {stream: Box::new(FastaStream::<File>::new_from_file(filename))};
-        } else{
+    pub fn new(filename: &String) -> SeqReader {
+        if filename.ends_with("fastq.gz") {
+            return SeqReader {
+                stream: Box::new(FastqStream::<GzDecoder<File>>::new_from_file(filename)),
+            };
+        } else if filename.ends_with("fastq") {
+            return SeqReader {
+                stream: Box::new(FastqStream::<File>::new_from_file(filename)),
+            };
+        } else if filename.ends_with("fna.gz") {
+            return SeqReader {
+                stream: Box::new(FastaStream::<GzDecoder<File>>::new_from_file(filename)),
+            };
+        } else if filename.ends_with("fna") {
+            return SeqReader {
+                stream: Box::new(FastaStream::<File>::new_from_file(filename)),
+            };
+        } else {
             panic!("Could not determine the format of file {}", filename);
         }
     }
-    
+
     // New from stdin
-    pub fn new_from_stdin(fastq: bool, gzipped: bool) -> SeqReader{
+    pub fn new_from_stdin(fastq: bool, gzipped: bool) -> SeqReader {
         if fastq && gzipped {
-            return SeqReader {stream: Box::new(FastqStream::<GzDecoder<std::io::Stdin>>::new(GzDecoder::new(io::stdin())))};
-        } else if fastq && !gzipped{
-            return SeqReader {stream: Box::new(FastqStream::<std::io::Stdin>::new(io::stdin()))};
-        } else if !fastq && gzipped{
-            return SeqReader {stream: Box::new(FastaStream::<GzDecoder<std::io::Stdin>>::new(GzDecoder::new(io::stdin())))};
-        } else if !fastq && !gzipped{
-            return SeqReader {stream: Box::new(FastaStream::<std::io::Stdin>::new(io::stdin()))};
-        } else{
+            return SeqReader {
+                stream: Box::new(FastqStream::<GzDecoder<std::io::Stdin>>::new(
+                    GzDecoder::new(io::stdin()),
+                )),
+            };
+        } else if fastq && !gzipped {
+            return SeqReader {
+                stream: Box::new(FastqStream::<std::io::Stdin>::new(io::stdin())),
+            };
+        } else if !fastq && gzipped {
+            return SeqReader {
+                stream: Box::new(FastaStream::<GzDecoder<std::io::Stdin>>::new(
+                    GzDecoder::new(io::stdin()),
+                )),
+            };
+        } else if !fastq && !gzipped {
+            return SeqReader {
+                stream: Box::new(FastaStream::<std::io::Stdin>::new(io::stdin())),
+            };
+        } else {
             panic!("This line should never be reached");
         }
     }
-    
-    pub fn read_all(&mut self){
+
+    pub fn read_all(&mut self) {
         self.stream.read_all();
     }
 }
 
-
-fn main(){
+fn main() {
     let matches = Command::new("Fasta/fastq parsing")
         .version("0.1.0")
         .author("Jarno N. Alanko <alanko.jarno@gmail.com>")
         .about("Fasta/fastq parsing")
-        .arg(Arg::new("input")
-                 .short('i')
-                 .long("input")
-                 .help("Input filename"))
-        .arg(Arg::new("fasta")
-                 .short('a')
-                 .long("fasta")
-                 .action(ArgAction::SetTrue)
-                 .help("Parse in fasta format"))
-        .arg(Arg::new("fastq")
-                 .short('q')
-                 .long("fastq")
-                 .action(ArgAction::SetTrue)
-                 .help("Parse in fastq format"))
-        .arg(Arg::new("gzip")
-                 .short('g')
-                 .long("gzip")
-                 .action(ArgAction::SetTrue)
-                 .help("Read gzipped input"))
+        .arg(
+            Arg::new("input")
+                .short('i')
+                .long("input")
+                .help("Input filename"),
+        )
+        .arg(
+            Arg::new("fasta")
+                .short('a')
+                .long("fasta")
+                .action(ArgAction::SetTrue)
+                .help("Parse in fasta format"),
+        )
+        .arg(
+            Arg::new("fastq")
+                .short('q')
+                .long("fastq")
+                .action(ArgAction::SetTrue)
+                .help("Parse in fastq format"),
+        )
+        .arg(
+            Arg::new("gzip")
+                .short('g')
+                .long("gzip")
+                .action(ArgAction::SetTrue)
+                .help("Read gzipped input"),
+        )
         .get_matches();
-        
-    if let Some(infile) = matches.get_one::<String>("input"){
+
+    if let Some(infile) = matches.get_one::<String>("input") {
         let mut reader = SeqReader::new(&infile);
         reader.read_all();
-    } else{
+    } else {
         let is_fasta = matches.get_flag("fasta");
         let is_fastq = matches.get_flag("fastq");
         let is_gzip = matches.get_flag("gzip");
@@ -92,7 +118,9 @@ fn main(){
             std::process::exit(-1)
         }
         if !is_fasta && !is_fastq {
-            println!("Error: must give --fasta or --fastq and possibly --gzip if reading from stdin.");
+            println!(
+                "Error: must give --fasta or --fastq and possibly --gzip if reading from stdin."
+            );
             std::process::exit(-1)
         }
         let mut reader = SeqReader::new_from_stdin(is_fastq, is_gzip);
