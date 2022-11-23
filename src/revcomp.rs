04dddf57fee9f21ca70d1d8900ea462c21d03466
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-11-23 23:35:24 +0200

    Works

--- src/revcomp.rs
+++ src/revcomp.rs
@@ -16,11 +16,9 @@ use std::path::Path;
 fn main() {
     let args: Vec<String> = env::args().collect();
     //let mut m_args = args.clone();
-    let seq = args[1].clone();
-    let mut bytes = seq.into_bytes();
+    let seq = args[1].clone(); // Take a shallow copy of the string
+    let mut bytes = seq.into_bytes(); // Turn the string into a byte vector (consumes string)
     let n = bytes.len();
-
-    dbg!(n);
 
     // Reverse
     for i in 0..(n/2){
@@ -28,8 +26,6 @@ fn main() {
         bytes[i] = bytes[n-1-i];
         bytes[n-1-i] = temp;
     }
-
-    //dbg!(bytes);
 
     // Complement
     for i in 0..n {
@@ -46,7 +42,8 @@ fn main() {
             bytes[i] = b'A';
         }
         else { 
-            //bytes[i] = b'X';
+            println!("Error: non-ACGT character: '{}'", bytes[i] as char);
+            return;
         }
     }
     println!("{}", std::str::from_utf8(bytes.as_slice()).expect(""));
