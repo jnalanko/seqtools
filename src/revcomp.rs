c897cf6605cc4ad85c7e4c9fa1dd3e181c7c3ece
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-11-23 23:30:10 +0200

    Array

--- src/revcomp.rs
+++ src/revcomp.rs
@@ -15,13 +15,39 @@ use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let bytes = args[1].as_bytes();
+    //let mut m_args = args.clone();
+    let seq = args[1].clone();
+    let mut bytes = seq.into_bytes();
     let n = bytes.len();
-    for i in 0..bytes.len(){
-        if bytes[n-1-i] == ('A' as u8){print!("{}",'T')};
-        if bytes[n-1-i] == ('C' as u8){print!("{}",'G')};
-        if bytes[n-1-i] == ('G' as u8){print!("{}",'C')};
-        if bytes[n-1-i] == ('T' as u8){print!("{}",'A')};
+
+    dbg!(n);
+
+    // Reverse
+    for i in 0..(n/2){
+        let temp = bytes[i];
+        bytes[i] = bytes[n-1-i];
+        bytes[n-1-i] = temp;
     }
-    println!();
+
+    //dbg!(bytes);
+
+    // Complement
+    for i in 0..n {
+        if bytes[i] == (b'A'){
+            bytes[i] = b'T';
+        }
+        else if bytes[i] == (b'C'){
+            bytes[i] = b'G';
+        }
+        else if bytes[i] == (b'G'){
+            bytes[i] = b'C';
+        }
+        else if bytes[i] == (b'T'){
+            bytes[i] = b'A';
+        }
+        else { 
+            //bytes[i] = b'X';
+        }
+    }
+    println!("{}", std::str::from_utf8(bytes.as_slice()).expect(""));
 }
