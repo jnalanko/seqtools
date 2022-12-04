024fadc3b90704211947fd4b92133cdd3af8c89b
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-12-03 20:43:10 -0600

    Bash autocompletes

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use clap::{Arg, ArgAction, Command};
 use flate2::read::MultiGzDecoder;
 use std::fs::File;
 use std::io::{self, Write};
-use std::io::BufReader;
 use std::io::Read;
 
+mod cli;
 
 // Fasta or fastq stream
 pub trait SeqStream {
@@ -32,7 +32,7 @@ struct SeqReader {
 impl SeqReader {
 
     // Need to constrain + 'static because boxed things always need to have a static
-    // lifetime
+    // lifetime.
     pub fn new_from_input_stream<R: Read + 'static>(r: R, mode: InputMode) -> Self{
         let reader = FastXReader::<R>::new(r, mode);
         SeqReader {stream: Box::new(reader)}
@@ -134,60 +134,7 @@ fn get_reader(input: ReaderInput) -> SeqReader{
 
 fn main() {
 
-    let matches = Command::new("Fasta/fastq parsing")
-        .version("0.1.0")
-        .author("Jarno N. Alanko <alanko.jarno@gmail.com>")
-        .about("Fasta/fastq parsing")
-        .arg(
-            Arg::new("input")
-                .short('i')
-                .long("input")
-                .help("Input filename")
-                .global(true),
-        )
-        .arg(
-            Arg::new("fasta")
-                .short('a')
-                .long("fasta")
-                .action(ArgAction::SetTrue)
-                .help("Parse in fasta format")
-                .global(true),
-        )
-        .arg(
-            Arg::new("fastq")
-                .short('q')
-                .long("fastq")
-                .action(ArgAction::SetTrue)
-                .help("Parse in fastq format")
-                .global(true),
-        )
-        .arg(
-            Arg::new("gzip")
-                .short('g')
-                .long("gzip")
-                .action(ArgAction::SetTrue)
-                .help("Read gzipped input")
-                .global(true),
-        ).subcommand(Command::new("length-histogram")
-            .about("Print the length histogram of the sequences.")
-            .arg(
-                Arg::new("min")
-                    .default_value("0")
-                    .help("Minimum value")
-            ).arg(
-                Arg::new("max")
-                    .default_value("1000")
-                    .help("Maximum value")
-            ).arg(
-                Arg::new("nbins")
-                    .default_value("20")
-                    .help("Number of bins")
-            )
-        ).subcommand(Command::new("stats")
-            .about("Print stats about the input.")
-        )
-        .get_matches();
-
+    let matches = cli::build_cli().get_matches();
 
     // Flag consistency check
     let mut reader = if let Some(infile) = matches.get_one::<String>("input") {
