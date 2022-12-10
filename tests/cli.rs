d3209fcbbee8572990e84a496cd523cc43b04b51
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-12-10 00:16:43 -0600

    Convert test written. Fails

--- tests/cli.rs
+++ tests/cli.rs
@@ -25,21 +25,59 @@ fn stats() -> Result<(), Box<dyn std::error::Error>> {
 fn convert() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("seqtools")?;
 
+    // fasta -> fasta.gz
     let first = cmd.arg("convert").arg("-i").arg("tests/data/reads.fastq.gz").arg("--fasta-out").arg("--gzip-out").stdout(Stdio::piped()).spawn()?;
     let first_stdout_result = first.wait_with_output()?.stdout;
 
-    let mut cmd = Command::cargo_bin("seqtools")?;
+    // fasta.gz -> fastq
+    cmd = Command::cargo_bin("seqtools")?;
     let mut second = cmd.arg("convert").arg("--fasta-in").arg("--gzip-in").arg("--fastq-out").stdin(Stdio::piped()).stdout(Stdio::piped()).spawn()?;
-    let mut second_stdin = second.stdin.as_mut().unwrap();
+    let second_stdin = second.stdin.as_mut().unwrap();
     second_stdin.write_all(&first_stdout_result)?;
     drop(second_stdin); // Flush
-
     let second_stdout_result = second.wait_with_output()?.stdout;
-    let S = str::from_utf8(&second_stdout_result)?;
-    println!("{}",S);
 
+    // fastq -> fastq.gz
+    cmd = Command::cargo_bin("seqtools")?;
+    let mut third = cmd.arg("convert").arg("--fastq-in").arg("--fastq-out").arg("--gzip-out").stdin(Stdio::piped()).stdout(Stdio::piped()).spawn()?;
+    let third_stdin = third.stdin.as_mut().unwrap();
+    third_stdin.write_all(&second_stdout_result)?;
+    drop(third_stdin); // Flush
+    let third_stdout_result = third.wait_with_output()?.stdout;
 
+    // fastq.gz -> fasta
+    cmd = Command::cargo_bin("seqtools")?;
+    let mut fourth = cmd.arg("convert").arg("--fastq-in").arg("--gzip-in").arg("--fasta-out").stdin(Stdio::piped()).stdout(Stdio::piped()).spawn()?;
+    let fourth_stdin = fourth.stdin.as_mut().unwrap();
+    fourth_stdin.write_all(&third_stdout_result)?;
+    drop(fourth_stdin); // Flush
+    let fourth_stdout_result = fourth.wait_with_output()?.stdout;
 
+    // See if we have the same fasta data as we started with
+    let S = str::from_utf8(&fourth_stdout_result)?;
+    let answer = "\
+>SRR403017.1 HWUSI-EAS108E_0007:3:1:3797:973/1
+TTGGACCGGCGCAAGACGGACCAGNGCGAAAGCATTTGCCAAGAANNNN
+>SRR403017.2 HWUSI-EAS108E_0007:3:1:10327:976/1
+CAACTTTCTATCTGGCATTCCCTGNGGAGGAAATAGAATCGCNNNN
+>SRR403017.3 HWUSI-EAS108E_0007:3:1:13569:972/1
+GATCGGAAGAGCACACGTCTGAACNCCAGTCACTTAGGCATCTCGNNNN
+>SRR403017.4 HWUSI-EAS108E_0007:3:1:16652:968/1
+TCTGTTATGTCGGATTACTGCTGTNAGTCAGTGNNNNN
+>SRR403017.5 HWUSI-EAS108E_0007:3:1:6652:978/1
+CTGTGTTTTTATTTAGTGGGTAGCNGAAGTTGTTCAGAAGAGCAGNNNN
+>SRR403017.6 HWUSI-EAS108E_0007:3:1:8133:984/1
+TGGAGCCCTTAAAAACGTACAGGCNCCTGGGGTCATTGGGGGTGAGGTNNNN
+>SRR403017.7 HWUSI-EAS108E_0007:3:1:8545:977/1
+TTAGATGTCCGGGGCTGCACGTGCNCTATGACTGGCTCAGCGNNNN
+>SRR403017.8 HWUSI-EAS108E_0007:3:1:8825:983/1
+ATAGAGAAGGGGGACAATGAGCCTGGATCTTTGCCTTGNNNN
+>SRR403017.9 HWUSI-EAS108E_0007:3:1:11308:980/1
+AAATTTATTTCTCATAGTTCTGGANTCTAGGAAGTTCAAGATCAGNNNN
+>SRR403017.10 HWUSI-EAS108E_0007:3:1:14685:981/1
+CCCATTCTTGGAGATACCAGCAAAAATTCNAATTCACCAACACCAGCAGCNNNN";
+
+    assert_eq!(S, answer);
     Ok(())
 }
 
