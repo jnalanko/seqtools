ff71bde2fc2cfe981fe5ae604b4f3d1d8da66c26
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-12-09 23:18:26 -0600

    Update test

--- tests/cli.rs
+++ tests/cli.rs
@@ -7,12 +7,13 @@ use predicates::prelude::*; // Used for writing assertions
 fn test_stats() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("seq_tools")?;
 
-    cmd.arg("stats").arg("-i").arg("reads.fna");
+    cmd.arg("stats").arg("-i").arg("tests/data/reads.fastq.gz");
     cmd.assert()
-        .stdout(predicate::str::contains("Number of nucleotides: 490"))
+        .stdout(predicate::str::contains("Number of nucleotides: 474"))
         .stdout(predicate::str::contains("Number of sequences: 10"))
-        .stdout(predicate::str::contains("Minimum sequence length: 49"))
-        .stdout(predicate::str::contains("Average sequence length: 49"));
+        .stdout(predicate::str::contains("Maximum sequence length: 54"))
+        .stdout(predicate::str::contains("Minimum sequence length: 38"))
+        .stdout(predicate::str::contains("Average sequence length: 47.4"));
 
     Ok(())
 }
