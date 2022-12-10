b1f543846950fc7ef7dec2fddaf37189cb909784
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-12-09 23:37:27 -0600

    Fix trim test

--- tests/cli.rs
+++ tests/cli.rs
@@ -4,7 +4,7 @@ use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 
 #[test]
-fn test_stats() -> Result<(), Box<dyn std::error::Error>> {
+fn stats() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("seq_tools")?;
 
     cmd.arg("stats").arg("-i").arg("tests/data/reads.fastq.gz");
@@ -18,51 +18,45 @@ fn test_stats() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn test_trim() -> Result<(), Box<dyn std::error::Error>> {
+#[test]
+fn trim() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("seq_tools")?;
 
     cmd.arg("trim").arg("-i").arg("tests/data/reads.fastq.gz")
-        .arg("from-start").arg("20").arg("from-end").arg("22").arg("--fastq-out");
+        .arg("--from-start").arg("20").arg("--from-end").arg("26").arg("--fastq-out");
+    // Corner case: 20 + 26 = 46. There is a sequence of length exactly 46. That should be deleted.
 
-    let answer = "@SRR403017.1 HWUSI-EAS108E_0007:3:1:3797:973/1
-    CCAGNGC
-    +
-    56789:;
-    @SRR403017.2 HWUSI-EAS108E_0007:3:1:10327:976/1
-    CCTG
-    +
-    fghi
-    @SRR403017.3 HWUSI-EAS108E_0007:3:1:13569:972/1
-    GAACNCC
-    +
-    IIIIIII
-    @SRR403017.5 HWUSI-EAS108E_0007:3:1:6652:978/1
-    TAGCNGA
-    +
-    IIIIIII
-    @SRR403017.6 HWUSI-EAS108E_0007:3:1:8133:984/1
-    AGGCNCCTGG
-    +
-    IIIIIIIIII
-    @SRR403017.7 HWUSI-EAS108E_0007:3:1:8545:977/1
-    GTGC
-    +
-    IIII
-    @SRR403017.9 HWUSI-EAS108E_0007:3:1:11308:980/1
-    TGGANTC
-    +
-    IIIIIII
-    @SRR403017.10 HWUSI-EAS108E_0007:3:1:14685:981/1
-    CAAAAATTCNAA
-    +
-    IIIIIIIIIIII
-    ";
+    let answer = 
+    "@SRR403017.1 HWUSI-EAS108E_0007:3:1:3797:973/1
+CCA
++
+567
+@SRR403017.3 HWUSI-EAS108E_0007:3:1:13569:972/1
+GAA
++
+III
+@SRR403017.5 HWUSI-EAS108E_0007:3:1:6652:978/1
+TAG
++
+III
+@SRR403017.6 HWUSI-EAS108E_0007:3:1:8133:984/1
+AGGCNC
++
+IIIIII
+@SRR403017.9 HWUSI-EAS108E_0007:3:1:11308:980/1
+TGG
++
+III
+@SRR403017.10 HWUSI-EAS108E_0007:3:1:14685:981/1
+CAAAAATT
++
+IIIIIIII";
 
-    let deleted_seqs_message = "Deleted 2 sequences as too short to trim";
+    let deleted_seqs_message = "Deleted 4 sequences as too short to trim";
 
     cmd.assert()
         .stdout(predicate::str::contains(answer))
-        .stdout(predicate::str::contains(deleted_seqs_message));
+        .stderr(predicate::str::contains(deleted_seqs_message));
 
     Ok(())
 }
