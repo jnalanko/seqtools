c1478242b4aa0664d2416ee41354350eb787aa7c
Jarno N. Alanko <alanko.jarno@gmail.com>
2025-01-10 11:24:32 -0600

    Test becomes to short

--- src/trim_adapters.rs
+++ src/trim_adapters.rs
@@ -145,7 +145,7 @@ mod tests {
         }
 
         { // Test matches too far from ends
-            let mut reader = jseqio::reader::DynamicFastXReader::new(Cursor::new(input_fasta)).unwrap();
+            let mut reader = jseqio::reader::DynamicFastXReader::new(Cursor::new(input_fasta.clone())).unwrap();
             let mut writer = TestWriter{records: vec![]};
             trim_adapters(&mut reader, &mut writer, vec![left_adapter.to_vec(), right_adapter.to_vec()], 1, 10, 0.65);
 
@@ -153,6 +153,14 @@ mod tests {
 
             eprintln!("Trimmed: {}", std::str::from_utf8(&writer.records.first().unwrap().seq).unwrap());
             assert_eq!(writer.records.first().unwrap().seq, s1); // Unchanged from original
+        }
+
+        { // Test becomes too short
+            let mut reader = jseqio::reader::DynamicFastXReader::new(Cursor::new(input_fasta)).unwrap();
+            let mut writer = TestWriter{records: vec![]};
+            trim_adapters(&mut reader, &mut writer, vec![left_adapter.to_vec(), right_adapter.to_vec()], 50, 100, 0.65);
+
+            assert_eq!(writer.records.len(), 0); // Was filtered out
         }
 
     }
